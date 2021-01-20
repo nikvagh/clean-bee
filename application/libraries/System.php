04182dca57d1a9f8f8d4fb219c93fe2d1d0afbb3
nikvagh <67430636+nikvagh@users.noreply.github.com>
2021-01-20 11:20:42 +0530

    ...

--- application/libraries/System.php
+++ application/libraries/System.php
@@ -5,6 +5,8 @@
 		{
 			$this->obj =& get_instance();
 			$this->get_config();
+
+			$this->obj->db->query("SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))");
 		}
 		
 		function get_config()
